2a0495ffa5219a56798caf39ced37078576137dd
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2024-02-14 17:47:08 +0530

    width added

--- app/components/UserList.tsx
+++ app/components/UserList.tsx
@@ -12,7 +12,7 @@ export default function UserList() {
     fetch(`https://api.github.com/users?per_page=100&&page=${currentPage}`, {
       headers: {
         Authorization:
-          'token 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',
+          'token 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',
       },
     })
       .then((res) => res.json())
@@ -29,7 +29,7 @@ export default function UserList() {
 
   return (
     <div>
-      <div className="flex justify-center text-center items-center m-2 p-2">
+      {users.length > 0 ? (<div className="flex justify-center text-center items-center m-2 p-2">
         <button
           className="p-2 bg-blue-300 text-black-300 rounded-lg m-1"
           onClick={() => setCurrentPage(currentPage - 1)}
@@ -44,7 +44,8 @@ export default function UserList() {
           Next
         </button>
         Total:{users.length / 10}
-      </div>
+      </div>):<div>Loading...</div>}
+      
       <div className="m-2 grid grid-cols-4 justify-between">
         {users &&
           users.length > 0 &&
@@ -58,6 +59,8 @@ export default function UserList() {
                 <Image
                   src={item && item['avatar_url'] ? item['avatar_url'] : ''}
                   className="object-none h-48 w-96"
+                  width={100}
+                  height={100}
                   alt={item['login']}
                 />
 
